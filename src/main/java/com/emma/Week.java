00e5263062c5058d44f7efb53ba8655664ac65a6
emmalanza <emmalanza@hotmail.com>
2025-01-31 13:51:44 +0100

    "initial version"

---
+++ src/main/java/com/emma/Week.java
@@ -0,0 +1,20 @@
+package com.emma;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Week {
+
+    private List<String> days;
+
+    public Week() {
+        this.days = new ArrayList<>();
+    }
+
+    public void createListOfDays() {}
+    public List<String> getDaysOfWeek() {}
+    public int getListSize() {}
+    public boolean deleteDay(String day) {}
+    public void sort() {}
+    public void clearList() {}
+
+}
